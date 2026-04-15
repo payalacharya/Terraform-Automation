023b05193c0c9f188479e9b77b3a54481f7d6263
payalacharya <payalaacharya00@gmail.com>
2026-04-15 20:26:55 +0530

    Initial commit for Terraform-automation

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-123"
+    bucket = "young-minds-payal-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "my-s3-bucket" {
+resource "aws_s3_bucket" "young-minds-payal-bucket" {
   bucket_prefix = var.bucket_prefix
   tags = var.tags
 }
 
 
-resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
+resource "aws_s3_bucket_versioning" "young-minds-payal-bucket-versioning" {
+  bucket = aws_s3_bucket.young-minds-payal-bucket.id
   versioning_configuration {
     status = var.versioning ? "Enabled" : "Suspended"
   }
